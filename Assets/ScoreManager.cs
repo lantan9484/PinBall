373c38fd99351cf8b86ad687107f1ff0b8a529a1
奥田誠 <lantan9484@gmail.com>
2019-04-29 14:05:05 +0900

    PinBall 課題：得点を表示しよう
    カリキュラムLesson5:3Dゲームアプリを作ろう Vol.1の提出課題です。
    SmallStarに当たると10点、LargeStar・SmallCloud・LargeCloudはそれぞれ50、30、100点がスコアに加算されるようになってます。

---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour {
+
+	private int score;
+	private GameObject scoreText;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		//シーン中のScoreTextオブジェクトを取得
+		this.scoreText = GameObject.Find("ScoreText");
+	}
+
+	void OnCollisionEnter(Collision other){
+		string ScoreTag = other.gameObject.tag;
+		if (ScoreTag == "SmallStarTag") {
+			//Debug.Log("衝突！");
+			score += 10;
+		} else if (ScoreTag == "LargeStarTag") {
+			score += 50;
+		} else if (ScoreTag == "SmallCloudTag") {
+			score += 30;
+		} else if (ScoreTag == "LargeCloudTag") {
+			score += 100;
+		} 
+
+	}
+		
+	// Update is called once per frame
+	void Update () {
+		//Debug.Log ("点数" + score);
+		this.scoreText.GetComponent<Text> ().text = "score:" + score;
+	}
+
+}
